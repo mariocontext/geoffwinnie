9bfcdd0bd48a921be0ae1fc0629feace0dd121c6
Mario Noble <mario@contextmultimedia.com>
2012-09-29 14:08:13 -0700

    adding initial resources

---
+++ config/site.php
@@ -0,0 +1,10 @@
+<?php 
+define('DB_SERVER', 'mysql.geoffwinnie.com');
+define('DB_USERNAME', 'geoffwinnie2');
+define('DB_PASSWORD', 'groucho2');
+define('DB_DATABASE', 'mydomains_geoffwinnie');
+define('BASE_URL', 'http://geoffwinnie.com');
+define('DIR_REL', '');
+define('PERMISSIONS_MODEL', 'advanced');
+define('PASSWORD_SALT', '3RUxjD6a0Khov5AGSIpOYwzfXmZHekMgELb4JNrnq289iFW1tPQCluTyscV7B');
+?>
